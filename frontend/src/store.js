615de095a6efa129d1fc1392e989cb9db5f8fb9c
Kirk Vander Kuy <kvk807@gmail.com>
2020-10-15 10:18:45 -0500

    added redux, created product list action and reducer

---
+++ frontend/src/store.js
@@ -0,0 +1,21 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { productListReducer } from './reducers/productReducers';
+
+const reducer = combineReducers({
+  productList: productListReducer,
+});
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
