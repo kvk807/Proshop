8650e413b28b32552d5ec7ca7dc5d757168de60a
Kirk Vander Kuy <kvk807@gmail.com>
2020-11-29 17:25:46 -0600

    40 - add json web token for protected routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,10 @@
 import asyncHandler from 'express-async-handler';
+import generateToken from '../utils/generateToken.js';
 import User from '../models/userModel.js';
 
 //  @desc    Authenticate user & get token
 //  @route   POST /api/users/login
-//  @access
+//  @access  Public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
@@ -15,12 +16,31 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     });
   } else {
-    res.status(401);
+    res.status(401); // Unauthorized
     throw new Error('Invalid email or password');
   }
 });
 
-export { authUser };
+//  @desc    Get user profile
+//  @route   GET /api/users/lprofile
+//  @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+export { authUser, getUserProfile };
